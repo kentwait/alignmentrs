b322a8dac03581f5367d1dd085b3c6171f410b0a
kent <kentkawashima@gmail.com>
2019-01-29 18:27:03 +0900

    Updates __init__ imports

--- alignmentrs/__init__.py
+++ alignmentrs/__init__.py
@@ -1,8 +1,11 @@
-from libalignmentrs import alignment as libalignment
 from libalignmentrs import sequence as libsequence
-from libalignmentrs.alignment import AlignmentMatrix
+from libalignmentrs import marker as libmarker
 from libalignmentrs.sequence import Sequence
+from libalignmentrs.marker import Marker
+from alignmentrs.alignment import AlignmentMatrix
+from alignmentrs import alignment
 
 __author__ = 'Kent Kawashima'
 __version__ = '0.1.0'
-__all__ = ['libalignment', 'libsequence', 'AlignmentMatrix', 'Sequence']
+__all__ = ['libalignment', 'libsequence', 'Sequence', 'Marker',
+           'AlignmentMatrix', 'alignment']
