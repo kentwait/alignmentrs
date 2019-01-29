11c90ccaaba60241735478fb7926db517de649e7
kent <kentkawashima@gmail.com>
2019-01-29 19:45:52 +0900

    Updates imports to make more things visible at the top level

--- alignmentrs/__init__.py
+++ alignmentrs/__init__.py
@@ -2,10 +2,11 @@ from libalignmentrs import sequence as libsequence
 from libalignmentrs import marker as libmarker
 from libalignmentrs.sequence import Sequence
 from libalignmentrs.marker import Marker
-from alignmentrs.alignment import AlignmentMatrix
-from alignmentrs import alignment
+from alignmentrs.alignment import AlignmentMatrix, Alignment
+from alignmentrs.fasta import fasta_file_to_list, fasta_file_to_alignment
 
 __author__ = 'Kent Kawashima'
 __version__ = '0.1.0'
 __all__ = ['libalignment', 'libsequence', 'Sequence', 'Marker',
-           'AlignmentMatrix', 'alignment']
+           'AlignmentMatrix', 'Alignment',
+           'fasta_file_to_list', 'fasta_file_to_alignment']
