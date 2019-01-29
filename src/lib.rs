d657945199803eb01fef3a49c7d0efca44614911
kent <kentkawashima@gmail.com>
2019-01-29 14:13:09 +0900

    Adds sequence and alignment modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#![feature(specialization)]
+
+#[macro_use]
+extern crate pyo3;
+
+pub mod alignment;
+pub mod sequence;
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }
+// }
