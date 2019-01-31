aa34c7f2772b830d21bcb2f4d32ab1ca14c18abe
kent <kentkawashima@gmail.com>
2019-01-31 19:20:23 +0900

    Changes version number to 0.3.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name='alignmentrs',
     author='Kent Kawashima',
-    version='0.2.1',
+    version='0.3.0',
     author_email='kentkawashima@gmail.com',
     description='Quickly read and manipulate multiple sequence alignments in Python',
     long_description=long_description,
