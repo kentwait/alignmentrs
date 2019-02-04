79703647394c70f1307a08d10690cb99ea4c0fb1
kent <kentkawashima@gmail.com>
2019-02-04 11:50:34 +0900

    Changes version to 0.4.0

--- alignmentrs/__init__.py
+++ alignmentrs/__init__.py
@@ -7,7 +7,7 @@ import libalignmentrs.sample as libsample
 import libalignmentrs.marker as libmarker
 
 __author__ = 'Kent Kawashima'
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 __all__ = ['libsample', 'libmarker', 'Sample', 'Marker',
            'BaseAlignment', 'Alignment',
            'fasta_file_to_alignment']
