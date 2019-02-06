954647c28ce34b5cd1383183148d3699838fba74
kent <kentkawashima@gmail.com>
2019-02-06 17:41:42 +0900

    Updates imports

--- alignmentrs/__init__.py
+++ alignmentrs/__init__.py
@@ -1,14 +1,13 @@
-from libalignmentrs.sample import Sample
-from libalignmentrs.marker import Marker
+from libalignmentrs.record import Record
 from libalignmentrs.alignment import BaseAlignment
 from alignmentrs.alignment import Alignment
 from alignmentrs.alignment import fasta_file_to_alignment
 from alignmentrs.set import AlignmentSet
-import libalignmentrs.sample as libsample
-import libalignmentrs.marker as libmarker
+import libalignmentrs.record as librecord
+
 
 __author__ = 'Kent Kawashima'
 __version__ = '0.6.2'
-__all__ = ['libsample', 'libmarker', 'Sample', 'Marker',
-           'BaseAlignment', 'Alignment', 'AlignmentSet',
+__all__ = ['librecord', 'Record', 'BaseAlignment',
+           'Alignment', 'AlignmentSet',
            'fasta_file_to_alignment']
