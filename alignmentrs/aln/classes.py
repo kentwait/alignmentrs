606e32facdb2ccc8bd7d158b7fa44fc4c799d7c2
kent <kentkawashima@gmail.com>
2019-02-07 16:26:30 +0900

    Comments out blocklists related code

--- alignmentrs/aln/classes.py
+++ alignmentrs/aln/classes.py
@@ -681,18 +681,19 @@ class Alignment:
             aln.markers.remove_sites(i)
             assert aln.samples.nsites == aln.markers.nsites, \
                 "Sample and marker nsites are not equal."
-        # Update blocks if exists
-        if aln.blocklists:
-            aln.blocklists = [
-                blockrs.remove_sites_from_blocks(blist, i)
-                for seq, blist in zip(aln.samples.sequences, aln.blocklists)]
-        # Update block data in description if description encoder is specified
-        if description_encoder:
-            aln.samples.set_descriptions(
-                list(range(aln.samples.nrows)),
-                [description_encoder(sid, blist)
-                 for sid, blist in zip(aln.samples.ids, aln.blocklists)]
-            )
+        # # Update blocks if exists
+        # if aln.blocklists:
+        #     aln.blocklists = [
+        #         blockrs.remove_sites_from_blocks(blist, i)
+        #         for seq, blist in zip(aln.samples.sequences, aln.blocklists)]
+        # # Update block data in description if description encoder is specified
+        # if description_encoder:
+        #     aln.samples.set_descriptions(
+        #         list(range(aln.samples.nrows)),
+        #         [description_encoder(sid, blist)
+        #          for sid, blist in zip(aln.samples.ids, aln.blocklists)]
+        #     )
+        aln._linspace.remove(i)
         if copy:
             return aln
 
@@ -734,19 +735,20 @@ class Alignment:
             aln.markers.retain_sites(i)
             assert aln.samples.nsites == aln.markers.nsites, \
                 "Sample and marker nsites are not equal."
-        # Update blocks if exists
-        if aln.blocklists:
-            j = [pos for pos in range(aln.samples.nsites) if pos not in i]
-            aln.blocklists = [
-                blockrs.remove_sites_from_blocks(blist, j)
-                for seq, blist in zip(aln.samples.sequences, aln.blocklists)]
-        # Update block data in description if description encoder is specified
-        if description_encoder:
-            aln.samples.set_descriptions(
-                list(range(aln.samples.nrows)),
-                [description_encoder(sid, blist)
-                 for sid, blist in zip(aln.samples.ids, aln.blocklists)]
-            )
+        # # Update blocks if exists
+        # if aln.blocklists:
+        #     j = [pos for pos in range(aln.samples.nsites) if pos not in i]
+        #     aln.blocklists = [
+        #         blockrs.remove_sites_from_blocks(blist, j)
+        #         for seq, blist in zip(aln.samples.sequences, aln.blocklists)]
+        # # Update block data in description if description encoder is specified
+        # if description_encoder:
+        #     aln.samples.set_descriptions(
+        #         list(range(aln.samples.nrows)),
+        #         [description_encoder(sid, blist)
+        #          for sid, blist in zip(aln.samples.ids, aln.blocklists)]
+        #     )
+        aln._linspace.retain(i)
         if copy:
             return aln
 
@@ -945,79 +947,79 @@ class Alignment:
 
     # Block-related methods
 
-    def set_blocklists(self, ref_seq, description_encoder=None):
-        """Creates new block information for the sequences given a reference.
+    # def set_blocklists(self, ref_seq, description_encoder=None):
+    #     """Creates new block information for the sequences given a reference.
 
-        Parameters
-        ----------
-        ref_seq : str
-            Reference sequence length must match alignment length.
-        description_encoder : function, optional
-            Function that uses the sample's name and list of blocks
-            to generate a string representation of the sample's block data.
-            If not specified, but site tracking is enabled, block data are
-            updated but the string representation in the description is not
-            updated. (default is None)
+    #     Parameters
+    #     ----------
+    #     ref_seq : str
+    #         Reference sequence length must match alignment length.
+    #     description_encoder : function, optional
+    #         Function that uses the sample's name and list of blocks
+    #         to generate a string representation of the sample's block data.
+    #         If not specified, but site tracking is enabled, block data are
+    #         updated but the string representation in the description is not
+    #         updated. (default is None)
 
-        """
-        self.blocklists = [blockrs.pairwise_to_blocks(ref_seq, seq)
-                           for seq in self.samples.sequences]
-        if description_encoder:
-            self.samples.set_descriptions(
-                list(range(self.samples.nrows)),
-                [description_encoder(sid, blist)
-                 for sid, blist in zip(self.samples.ids, self.blocklists)]
-            )
+    #     """
+    #     self.blocklists = [blockrs.pairwise_to_blocks(ref_seq, seq)
+    #                        for seq in self.samples.sequences]
+    #     if description_encoder:
+    #         self.samples.set_descriptions(
+    #             list(range(self.samples.nrows)),
+    #             [description_encoder(sid, blist)
+    #              for sid, blist in zip(self.samples.ids, self.blocklists)]
+    #         )
 
-    def parse_description_as_blocks(self, description_decoder=None):
-        """Parses sample description into block data.
+    # def parse_description_as_blocks(self, description_decoder=None):
+    #     """Parses sample description into block data.
 
-        Parameters
-        ----------
-        description_decoder : function
-            Function that locates the string representation of the list of
-            blocks from the text in the sample's description.
-            If not specified, the string after last under underscore "_" in
-            the description will be considered the stringed list of
-            blocks.
+    #     Parameters
+    #     ----------
+    #     description_decoder : function
+    #         Function that locates the string representation of the list of
+    #         blocks from the text in the sample's description.
+    #         If not specified, the string after last under underscore "_" in
+    #         the description will be considered the stringed list of
+    #         blocks.
 
-        """
-        if not self.blocklists:
-            self.blocklists = [None for _ in range(self.samples.nrows)]
-        for i, desc in enumerate(self.samples.descriptions):
-            if description_decoder:
-                desc = description_decoder(desc)
-            else:
-                desc = desc.split('_')[-1]
-            # Parse block str into blocks
-            self.blocklists[i] = blockrs.libblock.from_block_str(desc)
+    #     """
+    #     if not self.blocklists:
+    #         self.blocklists = [None for _ in range(self.samples.nrows)]
+    #     for i, desc in enumerate(self.samples.descriptions):
+    #         if description_decoder:
+    #             desc = description_decoder(desc)
+    #         else:
+    #             desc = desc.split('_')[-1]
+    #         # Parse block str into blocks
+    #         self.blocklists[i] = blockrs.libblock.from_block_str(desc)
 
-    def write_blocks_to_description(self, description_encoder):
-        """Writes each sample's block data as a string, replacing its
-        description.
+    # def write_blocks_to_description(self, description_encoder):
+    #     """Writes each sample's block data as a string, replacing its
+    #     description.
 
-        Parameters
-        ----------
-        description_encoder : function, optional
-            This function returns a formatted string encoding the block data.
-            The block string will replace the sample's description.
-            This function receives two parameters, the sample ID and the
-            sample's block data. (default is None)
+    #     Parameters
+    #     ----------
+    #     description_encoder : function, optional
+    #         This function returns a formatted string encoding the block data.
+    #         The block string will replace the sample's description.
+    #         This function receives two parameters, the sample ID and the
+    #         sample's block data. (default is None)
 
-        Raises
-        ------
-        TypeError
-            Given parameter has the wrong parameter type.
+    #     Raises
+    #     ------
+    #     TypeError
+    #         Given parameter has the wrong parameter type.
 
-        """
-        if self.blocklists:
-            self.samples.set_descriptions(
-                list(range(self.samples.nrows)),
-                [description_encoder(sid, blist)
-                 for sid, blist in zip(self.samples.ids, self.blocklists)]
-            )
-        else:
-            raise ValueError('Block list is empty')
+    #     """
+    #     if self.blocklists:
+    #         self.samples.set_descriptions(
+    #             list(range(self.samples.nrows)),
+    #             [description_encoder(sid, blist)
+    #              for sid, blist in zip(self.samples.ids, self.blocklists)]
+    #         )
+    #     else:
+    #         raise ValueError('Block list is empty')
 
     # Special methods
 
