85ccdaa9ab3a52cf429c79636b68f2b7ec39229e
kent <kentkawashima@gmail.com>
2019-02-07 14:35:04 +0900

    Adds LSpace object

--- src/position.rs
+++ src/position.rs
@@ -92,7 +92,10 @@ impl PyObjectProtocol for Block {
 
 #[pyclass(subclass)]
 #[derive(Clone)]
-pub struct Dimension {
+/// LSpace(init_state, start, stop)
+/// 
+/// LSpace represents a dimension in space.
+pub struct LSpace {
 
     // type_map: HashMap<i32, String>,
     blocks: Vec<[i32; 3]>,
@@ -100,51 +103,54 @@ pub struct Dimension {
 }
 
 #[pymethods]
-impl Dimension {
+impl LSpace {
     #[new]
-    /// Creates a new Dimension object from an id, and start and stop
+    /// Creates a new LSpace object from an init_state, and start and stop
     /// coordinates.
-    fn __new__(obj: &PyRawObject, init_type: i32, start: i32, stop: i32) -> PyResult<()> {
+    fn __new__(obj: &PyRawObject, init_state: i32, start: i32, stop: i32) -> PyResult<()> {
         if start > stop {
             return Err(exceptions::ValueError::py_err(
                 format!("start must be less than stop: {} !< {}",
                         start, stop)))
         }
         obj.init(|_| {
-            Dimension { 
-                blocks: vec![[init_type, start, stop]],
+            LSpace { 
+                blocks: vec![[init_state, start, stop]],
             }
         })
     }
 
-    fn remove(&mut self, ilist: Vec<i32>) -> PyResult<()> {
-        let mut ilist = ilist;
-        ilist.sort_unstable();
-        ilist.reverse();
-        for (i, pos) in ilist.iter().enumerate() {
+    /// Removes points in linear space given based on a list of coordinates.
+    fn remove(&mut self, coords: Vec<i32>) -> PyResult<()> {
+        let mut coords = coords;
+        coords.sort_unstable();
+        coords.reverse();
+        for (i, pos) in coords.iter().enumerate() {
             let pos = *pos;
             for j in (0..(self.blocks.len() - i)).rev() {
                 if pos >= self.blocks[j][1] && pos < self.blocks[j][2] {
                     // Remove block currently at j and get values
-                    let [t, start, stop] = self.blocks.remove(j);
+                    let [id, start, stop] = self.blocks.remove(j);
                     // Split this block at pos
                     // 10,11,12,13,14,15 : remove at index 2 (3rd pos)
                     // [_,10,16]
                     // 10,11,   13,14,15
                     // [_,10,12], [13:16]
                     // Insert two new blocks at j and j+1
-                    self.blocks.insert(j, [t, start, pos]);
-                    self.blocks.insert(j+1, [t, pos+1, stop]);
+                    self.blocks.insert(j, [id, start, pos]);
+                    self.blocks.insert(j+1, [id, pos+1, stop]);
                 }
             }
         }
         Ok(())
     }
 
-    fn retain(&mut self, ilist: Vec<i32>) -> PyResult<()> {
-        if let Some([_, _, last]) = self.blocks.last() {
-            let inverse_ilist: Vec<i32> = (0..*last)
-                                            .filter(|x| !ilist.contains(x))
+    /// Retains points in linear space specified by a
+    /// list of coordinates to keep.
+    fn retain(&mut self, coords: Vec<i32>) -> PyResult<()> {
+        if let Some([_, _, stop]) = self.blocks.last() {
+            let inverse_ilist: Vec<i32> = (0..*stop)
+                                            .filter(|x| !coords.contains(x))
                                             .collect();
             return self.remove(inverse_ilist)
         };
@@ -152,21 +158,61 @@ impl Dimension {
                                             dimension: block list is empty"))
     }
 
-    // min, max, full_len
-    fn min(&self) -> PyResult<i32> {
+    /// Inserts into the linear space at the given position.
+    fn insert(&mut self, pos: i32, state: i32, length: i32) -> PyResult<()> {
+        for i in (0..self.blocks.len()).rev() {
+            let [id, start, stop] = self.blocks[i];
+            if start >= pos && stop < pos {
+                if id == state {
+                    // Extend current block
+                    self.blocks[i] = [id, start, stop + length];
+                } else {
+                    // Create new state
+                    self.blocks.insert(i + 1, [id, stop, stop + length]);
+                }
+            }
+        }
+        Ok(())
+    }
+
+    /// Appends to the end of the linear space.
+    fn append(&mut self, state: i32, length: i32) -> PyResult<()> {
+        if let Some([id, start, stop]) = self.blocks.last() {
+            let (id, start, stop) = (*id, *start, *stop);
+            let i = self.blocks.len();
+            if id == state {
+                // Extend current block
+                self.blocks[i] = [id, start, stop + length];
+            } else {
+                // Create new state
+                self.blocks.push([id, stop, stop + length]);
+            }
+        } else {
+            self.blocks.push([state, 0, length]);
+        }
+        Ok(())
+    }
+
+    // start, stop, full_len
+
+    /// Returns the value at the start of the linear space.
+    fn start(&self) -> PyResult<i32> {
         match self.blocks.first() {
             Some([_, _, x]) => Ok(*x),
             None => return Err(exceptions::ValueError::py_err("cannot get minimum position: block list is empty"))
         }
     }
 
-    fn max(&self) -> PyResult<i32> {
+    /// Returns the end value of the linear space.
+    /// This value is not part of the space.
+    fn stop(&self) -> PyResult<i32> {
         match self.blocks.last() {
             Some([_, _, x]) => Ok(*x),
             None => return Err(exceptions::ValueError::py_err("cannot get maximum position: block list is empty"))
         }
     }
 
+    /// Returns the total length of the linear space.
     fn len_all(&self) -> PyResult<i32> {
         let mut length = 0;
         for [_, start, stop] in self.blocks.iter() {
@@ -176,6 +222,8 @@ impl Dimension {
         Ok(length)
     }
 
+    /// Returns the total length of the linear space where the
+    /// state is equal to 1.
     fn len_seq(&self) -> PyResult<i32> {
         let mut length = 0;
         for [id, start, stop] in self.blocks.iter() {
@@ -187,6 +235,8 @@ impl Dimension {
         Ok(length)
     }
 
+    /// Returns the total length of the linear space where the
+    /// state is equal to 0.
     fn len_gap(&self) -> PyResult<i32> {
         let mut length = 0;
         for [id, start, stop] in self.blocks.iter() {
@@ -198,9 +248,23 @@ impl Dimension {
         Ok(length)
     }
     
-
     // Format conversion
 
+    #[staticmethod]
+    /// Returns a linear space created using the given list of blocks.
+    fn from_blocks(blocks: Vec<&Block>) -> PyResult<LSpace> {
+        let mut data: Vec<[i32; 3]> = Vec::new();
+        for Block{ id, start, stop} in blocks.iter() {
+            let id: i32 = match id.parse() {
+                Ok(x) => x,
+                Err(_) => return Err(exceptions::TypeError::py_err("Cannot parse block id, maybe not i32?"))
+            };
+            data.push([id, *start, *stop]);
+        }
+        Ok(LSpace { blocks: data })
+    }
+
+    /// Returns the linear space as a list of blocks.
     fn to_blocks(&self) -> PyResult<Vec<Block>> {
         Ok(
             self.blocks.iter()
@@ -210,6 +274,9 @@ impl Dimension {
         )
     }
 
+    // TODO: from_array
+
+    /// Returns the linear space as a list of integer coordinates.
     fn to_array(&self) -> PyResult<Vec<i32>> {
         Ok(self._to_array())
     }
@@ -258,13 +325,13 @@ impl Dimension {
 }
 
 #[pyproto]
-impl PyObjectProtocol for Dimension {
+impl PyObjectProtocol for LSpace {
     fn __repr__(&self) -> PyResult<String> {
-        let min = match self.min() {
+        let start = match self.start() {
             Ok(x) => x,
             Err(x) => return Err(x)
         };
-        let max = match self.max() {
+        let stop = match self.stop() {
             Ok(x) => x,
             Err(x) => return Err(x)
         };
@@ -272,7 +339,7 @@ impl PyObjectProtocol for Dimension {
             Ok(x) => x,
             Err(x) => return Err(x)
         };
-        Ok(format!("Dimension(min={}, max={}, length={})", min, max, length))
+        Ok(format!("LSpace(start={}, stop={}, length={})", start, stop, length))
     }
     
     fn __str__(&self) -> PyResult<String> {
@@ -292,20 +359,20 @@ impl PyObjectProtocol for Dimension {
     }
 }
 
-impl Dimension {
+impl LSpace {
     fn _to_array(&self) -> Vec<i32> {
-        let mut ilist: Vec<i32> = Vec::new();
+        let mut coords: Vec<i32> = Vec::new();
         for [id, start, stop] in self.blocks.iter() {
             let (id, start, stop) = (*id, *start, *stop);
             if id == 1 {
                 let mut array: Vec<i32> = (start..stop).map(|x| x).collect();
-                ilist.append(&mut array);
+                coords.append(&mut array);
             } else if id == 0 {
                 let mut array: Vec<i32> = (start..stop).map(|_| -1).collect();
-                ilist.append(&mut array);
+                coords.append(&mut array);
             }
         }
-        ilist
+        coords
     }
 }
 
@@ -313,7 +380,7 @@ impl Dimension {
 #[pymodinit]
 fn position(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Block>()?;
-    m.add_class::<Dimension>()?;
+    m.add_class::<LSpace>()?;
 
     Ok(())
 }
