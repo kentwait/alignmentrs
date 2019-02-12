eb93b131ebe5eb8afae13fa2fc877e2452812640
kent <kentkawashima@gmail.com>
2019-02-12 16:03:26 +0900

    Removes unnecessary files

--- alignmentrs_imports.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('../')
-from os import getcwd
-from numpy import matrix, array, array_equal
-import nose 
-import random
-from classes import *
-from funcs import *
-from alignmentrs_test_funcs import *
