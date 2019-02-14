6369cdb0e3d3ab3196f42da44e27b741465da856
kent <kentkawashima@gmail.com>
2019-02-14 14:57:44 +0900

    Removes tests from rust code

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,3 @@ extern crate regex;
 pub mod alignment;
 pub mod record;
 pub mod position;
-
-// #[cfg(test)]
-// mod tests {
-//     #[test]
-//     fn it_works() {
-//         assert_eq!(2 + 2, 4);
-//     }
-// }
