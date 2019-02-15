7398e470b1f2e07defebd61652f88e99407d7309
Kent Kawashima <kentkawashima@gmail.com>
2019-02-15 09:55:31 +0900

    Updates imports

--- alignmentrs/aln/__init__.py
+++ alignmentrs/aln/__init__.py
@@ -1,6 +1,6 @@
-from alignmentrs.aln.classes import Alignment
+from alignmentrs.aln.classes import Alignment, CatAlignment
 from alignmentrs.aln.funcs import fasta_file_to_alignment
 
-__all__ = ['Alignment',
+__all__ = ['Alignment', 'CatAlignment'
            'fasta_file_to_alignment',
            ]
