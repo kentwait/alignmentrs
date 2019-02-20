9f237b3e4534de243098bc26a4a823cb58738cba
kent <kentkawashima@gmail.com>
2019-02-20 14:47:05 +0900

    Removes Record class

--- src/record.rs
+++ src/record.rs
@@ -2,112 +2,6 @@ use pyo3::prelude::*;
 use pyo3::{PyObjectProtocol, exceptions};
 
 
-#[pyclass(subclass)]
-#[derive(Clone)]
-/// Record(id, description, sequence)
-/// 
-/// Record represents a single sequence sample.
-pub struct Record {
-    #[prop(get,set)]
-    pub id: String,
-
-    #[prop(get,set)]
-    pub description: String,
-    
-    #[prop(get,set)]
-    pub sequence: String,
-
-}
-
-#[pymethods]
-impl Record {
-    #[new]
-    /// Creates a new Record object from sequence_id, sequence_description
-    /// and sequence_str.
-    fn __new__(obj: &PyRawObject, id: &str, description: &str, sequence_str: &str) -> PyResult<()> {
-        let sequence_str = String::from(sequence_str);
-        obj.init(|_| {
-            Record {
-                id: id.to_string(),
-                description: description.to_string(),
-                sequence: sequence_str.to_string(),
-            }
-        })
-    }
-
-    #[getter]
-    fn len(&self) -> PyResult<i32> {
-        Ok(self.sequence.chars().count() as i32)
-    }
-}
-
-// Customizes __repr__ and __str__ of PyObjectProtocol trait
-#[pyproto]
-impl PyObjectProtocol for Record {
-    fn __repr__(&self) -> PyResult<String> {
-        let desc:String = match self.description.chars().count() {
-            x if x > 20 => {
-                let mut desc:String = self.description.char_indices()
-                                        .filter(|(i, _)| *i < 17)
-                                        .map(|(_, c)| c)
-                                        .collect();
-                desc.push_str("...");
-                desc
-            },
-            _ => self.description.clone()
-        };
-        let seq: String = match self.sequence.chars().count() {
-            x if x >= 15 => {
-                let mut seq = String::new();
-                for (i, c) in self.sequence.char_indices() {
-                    if i < 6 || i >= x-6 {
-                        seq.push(c);
-                    } else if i >= 6 && i < 9 {
-                        seq.push('.');
-                    }
-                }
-                seq
-            },
-            _ => self.sequence.clone()
-        };
-        Ok(format!("Record(id=\"{id}\", sequence=\"{seq}\", len={seq_len}, \
-                   description=\"{desc}\")", 
-                   id=self.id, seq=seq,
-                   seq_len=self.sequence.len(), desc=desc))
-    }
-
-    fn __str__(&self) -> PyResult<String> {
-        if self.description.len() > 0 {
-            return Ok(format!(">{id} {desc}\n{seq_len}",
-                id=self.id,
-                desc=self.description,
-                seq_len=self.sequence.chars().count()))
-        }
-        return Ok(format!(">{id}\n{seq_len}",
-                id=self.id,
-                seq_len=self.sequence.chars().count()))        
-    }
-}
-
-#[pyclass(subclass)]
-#[derive(Clone)]
-/// BaseRecord(id, description, sequence)
-/// 
-/// BaseRecord represents a single sequence sample.
-pub struct BaseRecord {
-    #[prop(get,set)]
-    pub id: String,
-
-    #[prop(get,set)]
-    pub description: String,
-    
-    pub sequence: Vec<String>,
-
-    #[prop(get)]
-    pub chunk_size: i32,
-
-}
-
 #[pymethods]
 impl BaseRecord {
     #[new]
@@ -224,7 +118,6 @@ impl PyObjectProtocol for BaseRecord {
 // Register python functions to PyO3
 #[pymodinit]
 fn record(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<Record>()?;
     m.add_class::<BaseRecord>()?;
 
     Ok(())
