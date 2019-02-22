aa4cc57408589dbbfe080a5e7f86222ebcfd33f3
Kent Kawashima <kentkawashima@gmail.com>
2019-02-22 09:08:36 +0900

    Fixes chunking implementation

--- src/alignment.rs
+++ src/alignment.rs
@@ -416,7 +416,10 @@ impl BaseAlignment {
 
     pub fn get_chunk(&self, i: usize, chunk_size: usize) -> PyResult<Vec<String>> {
         let sequences: Vec<String> = self.records.iter()
-            .map(|rec| rec.sequence[i..i+chunk_size].join(""))
+            .map(|rec| {
+                let chars: Vec<char> = rec.sequence.join("").chars().collect();
+                chars[i..i+chunk_size].into_iter().collect::<String>()
+            })
             .collect();
         Ok(sequences)
     }
