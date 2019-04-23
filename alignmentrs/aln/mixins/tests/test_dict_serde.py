afc46017a9409ee64b00294774bbfacc5796e339
kent <kentkawashima@gmail.com>
2019-04-23 13:03:52 +0900

    Updates dict mixin test

--- alignmentrs/aln/mixins/tests/test_dict_serde.py
+++ alignmentrs/aln/mixins/tests/test_dict_serde.py
@@ -5,13 +5,23 @@ from alignmentrs.aln.mixins import serde
 from alignmentrs.aln.mixins.tests.mocks import MockData
 
 
-class MockClass(serde.DictSerdeMixin):
-    def __init__(self, records, name=None, index=None, comments=None, row_metadata=None, column_metadata=None, store_history=True, **kwargs):
+class MockAlignment(serde.DictSerdeMixin):
+    def __init__(self, matrix, name=None,
+                 row_metadata=None, col_metadata=None,
+                 row_ids:list=None, row_descriptions:list=None,
+                 col_ids:list=None, col_descriptions:list=None,
+                 aln_metadata:dict=None, store_history=True,
+                 **kwargs):
         self.name = name
-        self.index = index
-        self.comments = comments
-        self.row_metadata = row_metadata
-        self.column_metadata = column_metadata
+        self.alignment_metadata = aln_metadata
+        if row_metadata is None:
+            self.row_metadata = pd.DataFrame(row_descriptions, index=row_ids)
+        else:
+            self.row_metadata = row_metadata
+        if col_metadata is None:
+            self.column_metadata = pd.DataFrame(col_descriptions, index=col_ids)
+        else:
+            self.column_metadata = col_metadata
         self.store_history = store_history
         self.kwargs = kwargs
         self.data = MockData()
@@ -20,62 +30,31 @@ class MockClass(serde.DictSerdeMixin):
 class TestDictSerdeMixin:
 
     def setup(self):
-        self.records = [
-            Record('test1', 'description1', 'ATGCAT'),
-            Record('test2', 'description2', 'ATGGGT'),
-            Record('test3', 'description3', 'ATGAAT'),
-        ]
         self.name = 'mock_aln'
-        self.index = [0,1,2,3,4,5]
-
-        self.comments = {'test_comment': 'testing'}
-        self.comments_empty = {}
-        self.comments_none = None
-
-        self.row_metadata = pd.DataFrame(
-            {
-                'description': ['description1', 'description2', 'description3'],
-            },
-            index=['test1', 'test2', 'test3']
-        )
-        self.row_metadata_empty = pd.DataFrame({})
-        self.row_metadata_none = None
-
-        self.column_metadata = pd.DataFrame(
-            {
-                'a': [0,1,2,3,4,5],
-                'b': [10,11,12,13,14,15],
-            },
-            index=self.index
-        )
-        self.column_metdata_empty = pd.DataFrame({})
-        self.column_metdata_none = None
-    
-        self.sequences = [
-            'ATGCAT',
-            'ATGGGT',
-            'ATGAAT',
-        ]
-
+        self.alignment_metadata = {'comment1': 'This is a comment.'}
+        self.row_metadata = pd.DataFrame(None, index=['test1','test2','test3'])
+        self.column_metadata = pd.DataFrame(None, index=range(6))
+        self.store_history = True
         # self.kwargs = kwargs
+        self.matrix = MockData()
+        self.test_aln = MockAlignment(
+            self.matrix, name=self.name,
+            row_metadata=self.row_metadata,
+            col_metadata=self.column_metadata,
+            aln_metadata=self.alignment_metadata,
+            store_history=self.store_history,
+        )
 
     def teardown(self):
         pass
 
-    def test_to_dict_with_meta(self):
-        test_class = MockClass(
-            self.records, name=self.name,
-            index=self.index,
-            comments=self.comments,
-            row_metadata=self.row_metadata,
-            column_metadata=self.column_metadata,
-            store_history=True,
-        )
-        test_dict = test_class.to_dict(row_metadata=True, column_metadata=True)
+    def test_to_dict_with_row_col_meta(self):
+        test_dict = self.test_aln.to_dict(
+            row_metadata=True, column_metadata=True)
         exp_dict = {
             'name': self.name,
-            'data': self.sequences,
-            'comments': self.comments,
+            'data': self.matrix.data,
+            'alignment_metadata': self.alignment_metadata,
             'row_metadata': self.row_metadata.to_dict(orient='list'),
             'row_metadata_index': self.row_metadata.index.to_list(),
             'column_metadata': self.column_metadata.to_dict(orient='list'),
@@ -86,46 +65,28 @@ class TestDictSerdeMixin:
                 exp_dict, test_dict
             )
 
-    def test_to_dict_row_meta(self):
-        test_class = MockClass(
-            self.records, name=self.name,
-            index=self.index,
-            comments=self.comments,
-            row_metadata=self.row_metadata,
-            column_metadata=self.column_metadata,
-            store_history=True,
-        )
-        test_dict = test_class.to_dict(row_metadata=True, column_metadata=False)
+    def test_to_dict_with_row_meta(self):
+        test_dict = self.test_aln.to_dict(
+            row_metadata=True, column_metadata=False)
         exp_dict = {
             'name': self.name,
-            'data': self.sequences,
-            'comments': self.comments,
+            'data': self.matrix.data,
+            'alignment_metadata': self.alignment_metadata,
             'row_metadata': self.row_metadata.to_dict(orient='list'),
             'row_metadata_index': self.row_metadata.index.to_list(),
-            'column_metadata': None,
-            'column_metadata_index': self.column_metadata.index.to_list(),
         }
         assert exp_dict == test_dict, \
             "expected and test dictionaries are not the same: {} != {}".format(
                 exp_dict, test_dict
             )
     
-    def test_to_dict_col_meta(self):
-        test_class = MockClass(
-            self.records, name=self.name,
-            index=self.index,
-            comments=self.comments,
-            row_metadata=self.row_metadata,
-            column_metadata=self.column_metadata,
-            store_history=True,
-        )
-        test_dict = test_class.to_dict(row_metadata=False, column_metadata=True)
+    def test_to_dict_with_col_meta(self):
+        test_dict = self.test_aln.to_dict(
+            row_metadata=False, column_metadata=True)
         exp_dict = {
             'name': self.name,
-            'data': self.sequences,
-            'comments': self.comments,
-            'row_metadata': None,
-            'row_metadata_index': self.row_metadata.index.to_list(),
+            'data': self.matrix.data,
+            'alignment_metadata': self.alignment_metadata,
             'column_metadata': self.column_metadata.to_dict(orient='list'),
             'column_metadata_index': self.column_metadata.index.to_list(),
         }
@@ -137,21 +98,20 @@ class TestDictSerdeMixin:
     def test_from_dict(self):
         test_dict = {
             'name': self.name,
-            'data': self.sequences,
-            'comments': self.comments,
+            'data': self.matrix.data,
+            'alignment_metadata': self.alignment_metadata,
             'row_metadata': self.row_metadata.to_dict(orient='list'),
             'row_metadata_index': self.row_metadata.index.to_list(),
             'column_metadata': self.column_metadata.to_dict(orient='list'),
             'column_metadata_index': self.column_metadata.index.to_list(),
         }
-        test_class = MockClass.from_dict(test_dict)
-        exp_class = MockClass(
-            self.records, name=self.name,
-            index=self.index,
-            comments=self.comments,
+        test_class = MockAlignment.from_dict(test_dict)
+        exp_class = MockAlignment(
+            self.matrix, name=self.name,
             row_metadata=self.row_metadata,
-            column_metadata=self.column_metadata,
-            store_history=True,
+            col_metadata=self.column_metadata,
+            aln_metadata=self.alignment_metadata,
+            store_history=self.store_history,
         )
         assert type(exp_class) == type(test_class), \
             "expected and test classes are not the same: {} != {}".format(
